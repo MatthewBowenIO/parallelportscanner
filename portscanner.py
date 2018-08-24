2b529c6a3d1c950907ba959116377cc6a6b741d5
Matthew Bowen <matthewbowenrd@gmail.com>
2018-08-24 01:40:50 -0400

    Actually checking error messages really means a lot

--- portscanner.py
+++ portscanner.py
@@ -7,21 +7,23 @@ import multiprocessing
 import time, datetime
 import argparse
 
-def parallel_port_scan(cc, ip, port):
-    return Parallel(n_jobs = cc)(delayed(port_scan)(ip, i) for i in range(port))
+def parallel_port_scan(cc, ip, portStart, portEnd):
+    return Parallel(n_jobs = cc)(delayed(port_scan)(ip, i) for i in range(portStart, portEnd))
 
 def port_scan(ip, port):
-    print "{}: {}".format(ip, port)
+    print "{}: {}".format(str(ip), port)
     s = socket(AF_INET, SOCK_STREAM)
+    s.settimeout(.5)
     try:
-        s = s.connect(ip, port)
+        s = s.connect((str(ip), port))
 
-        with open("logs.txt", "w") as log:
+        with open("logs.txt", "a") as log:
             log.write("{}: Port open: {}\n".format(ip, port))
             log.close()
 
         print "{}: Port open: {}".format(ip, port)
     except Exception as e:
+        print e
         pass
 
 def main():
@@ -41,7 +43,8 @@ def main():
 
     start = time.time()
     for ip in ipRange:
-        parallel_port_scan(cc, ip, int(portEnd))
+        last_ip = ip
+        parallel_port_scan(cc, ip, int(portStart), int(portEnd))
         #for port in range(int(portStart), int(portEnd)):
             #port_scan(ip, port)
     print str(datetime.timedelta(seconds=int(time.time() - start)))
